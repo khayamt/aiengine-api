ac48751368f02953a59c7d0bb53601a89a1ba329
khayamt <khayam@k-polit.co.za>
2025-11-30 13:41:17 +0200

    Added eureka

--- src/main/java/za/co/kpolit/aiengine/AiengineApplication.java
+++ src/main/java/za/co/kpolit/aiengine/AiengineApplication.java
@@ -1,9 +1,13 @@
 package za.co.kpolit.aiengine;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class AiengineApplication {
 
 	public static void main(String[] args) {
